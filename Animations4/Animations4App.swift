183153f3c3d56971283dbe1b126e628f1fb6b8f5
aimee_t <aimeetemple14@gmail.com>
2024-04-29 15:02:40 -0400

    Initial commit

---
+++ Animations4/Animations4App.swift
@@ -0,0 +1,17 @@
+//
+//  Animations4App.swift
+//  Animations4
+//
+//  Created by Temple on 2024-04-29.
+//
+
+import SwiftUI
+
+@main
+struct Animations4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
